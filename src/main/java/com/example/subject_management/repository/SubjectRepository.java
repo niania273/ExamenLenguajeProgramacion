d79513044f14e7c58eb20ec8df92b0daa8fc03a0
fabiana <i202315244@cibertec.edu.pe>
2024-09-28 20:34:08 -0500

    Proyecto agregado

---
+++ src/main/java/com/example/subject_management/repository/SubjectRepository.java
@@ -0,0 +1,7 @@
+package com.example.subject_management.repository;
+
+import com.example.subject_management.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+}
